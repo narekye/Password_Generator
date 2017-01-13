dc0ddbe6b59fc7243afef6ebc0625f24e70c1c19
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-01-12 18:49:35 -0600

    First version of Password Generator

---
+++ Password_Genetator/PasswordGenerator/Password.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// This class library used for generate random passwords. 
+    /// Used all digits, uppercase & downcase letters.
+    /// Can send any parametr to 'Generate' method which will generate password with sended count length.
+    /// </summary>
+    public static class Password
+    {
+        private static string pass = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        public static string Generate(int length)
+        {
+            Random Rdpass = new Random();
+            string password = String.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                password += pass[Rdpass.Next(0, pass.Length)];
+            }
+            return password;
+        }
+    }
+}
---
+++ Password_Genetator/TesePasswordGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PasswordGenerator; // add this 'using' to use class library.
+
+namespace TestPasswordGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Parameter is lenght of your own password. Example 1.
+            string mypassword = Password.Generate(4);
+            Console.WriteLine(mypassword);
+            // Or second one example below. Example 2
+            Console.WriteLine(Password.Generate(12));
+            // Delay !!
+            Console.Read();
+        }
+    }
+}
