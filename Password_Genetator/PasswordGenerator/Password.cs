658d1e013f7c471f1e223ac2a57ad6ee85c5b0a0
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-01-15 15:56:34 -0600

    Versoin 1.1 added non-static class.

--- Password_Genetator/PasswordGenerator/Password.cs
+++ Password_Genetator/PasswordGenerator/Password.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
+
 namespace PasswordGenerator
 {
     /// <summary>
-    /// This class library used for generate random passwords. 
+    /// This class used for generate random passwords. 
     /// Used all digits, uppercase & downcase letters.
     /// Can send any parametr to 'Generate' method which will generate password with sended count length.
     /// </summary>
@@ -11,13 +13,40 @@ namespace PasswordGenerator
         private static string pass = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         public static string Generate(int length)
         {
+            if (length > pass.Length)
+            {
+                throw new ArgumentOutOfRangeException("No much characters");
+            }
             Random Rdpass = new Random();
-            string password = String.Empty;
+            string password = string.Empty;
             for (int i = 0; i < length; i++)
             {
                 password += pass[Rdpass.Next(0, pass.Length)];
             }
             return password;
+        }
+    }
+    /// <summary>
+    /// This class used for generate random passwords.
+    /// Used all digits,uppercase and downcase letters with all symbols.
+    /// With this class you can create the instance of HardPassword and use.
+    /// </summary>
+    public class HardPassword
+    {
+        private string pass = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()-_=+|}{}:;',./";
+        public string Generate(int length)
+        {
+            if (length>pass.Length)
+            {
+                throw new ArgumentOutOfRangeException("No much characters");
+            }
+            Random rdpass = new Random();
+            string password = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                password += pass[rdpass.Next(rdpass.Next(i, pass.Length))];
+            }
+            return password; 
         }
     }
 }
--- Password_Genetator/TesePasswordGenerator/Program.cs
+++ Password_Genetator/TesePasswordGenerator/Program.cs
@@ -7,11 +7,15 @@ namespace TestPasswordGenerator
     {
         static void Main()
         {
+            Console.WriteLine();
             //Parameter is lenght of your own password. Example 1.
-            string mypassword = Password.Generate(4);
-            Console.WriteLine(mypassword);
+            string mypassword = Password.Generate(45);
+            Console.WriteLine("\t First password with length 4 | {0}", mypassword);
             // Or second one example below. Example 2
-            Console.WriteLine(Password.Generate(12));
+            Console.WriteLine("\t Second password with length 12 | {0}", Password.Generate(12));
+            var mypass = new HardPassword();
+            Console.WriteLine("\t Instance created by non-static class | {0}" ,mypass.Generate(34));
+            mypass.Generate(12);
             // Delay !!
             Console.Read();
         }
